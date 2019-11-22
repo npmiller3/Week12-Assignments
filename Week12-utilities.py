5dd3acb4e18d0d367095d32b90b8f035b268833a
Nathan Miller <npmiller@mymail.mines.edu>
2019-11-21 20:39:43 -0700

    LoadFile Added

--- Week12-utilities.py
+++ Week12-utilities.py
@@ -5,3 +5,9 @@
 
 def PrintOutput(a):
     print('OUTPUT', a)
+
+def LoadFile(filename):
+    filename = open(filename, 'r+')
+    astring = filename.read()
+    alist = astring.splitlines()
+    return alist
